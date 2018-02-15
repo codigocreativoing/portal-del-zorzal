94f5e39ab14e8c75f305a8de8134d4c9403a365b
codigocreativoing <desarrollo@codigocreativo.com.co>
2018-02-15 15:54:23 -0500

    se agrega boton redes sociales y actualizan logos

--- src/js/components/youtube_video.js
+++ src/js/components/youtube_video.js
@@ -11,12 +11,12 @@
 *   @author Jonathan MirCha <jonmircha@gmail.com>
 *   @version 1.0.0
 */
-export default function youtubeVideo () {
+export default function youtubeVideo() {
   const d = document,
     w = window,
     mq = w.matchMedia('(min-width: 64em)'),
     youtube = d.querySelectorAll('.Youtube'),
-    youtubeWrapper =d.querySelectorAll('.Youtube-wrapper'),
+    youtubeWrapper = d.querySelectorAll('.Youtube-wrapper'),
     youtubeIds = [],
     youtubeIframe = []
 
@@ -24,14 +24,14 @@ export default function youtubeVideo () {
 
   //console.log( youtubeIds )
 
-  function showVideo (mq) {
+  function showVideo(mq) {
     if (mq.matches) {
       youtubeWrapper.forEach((video, index) => {
         video.innerHTML = `<iframe src="https://www.youtube.com/embed/${youtubeIds[index]}" frameborder="0"></iframe>`
       })
     } else {
       youtubeWrapper.forEach((video, index) => {
-        video.innerHTML = `<a href="https://www.youtube.com/watch?v=${youtubeIds[index]}" target="_blank"><i class="fa fa-youtube-play"></i> Ver Video</a>`
+        video.innerHTML = `<a href="https://www.youtube.com/watch?v=${youtubeIds[index]}" target="_blank"><i class="fa fa-youtube"></i> Ver Video</a>`
       })
     }
   }
